22b630dd9a655edd7aab0551bc8d1ef8ee5a5692
Radmila Gama <milaejared@outlook.com>
2021-01-21 17:18:32 -0300

    >:)

---
+++ Modulo 3/1- Aplicacao/Controllers/CategoriasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1__Aplicacao.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
