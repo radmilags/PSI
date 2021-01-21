4ed1821da989425c8da8f66500762542c3c08a09
Radmila Gama <milaejared@outlook.com>
2021-01-21 17:39:06 -0300

    ok

--- Modulo 3/1- Aplicacao/App_Start/RouteConfig.cs
+++ Modulo 3/1- Aplicacao/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _1__Aplicacao
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Categorias", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
