e4a6adb8d42bcda3ab70b5f27b81f8c88cdbd3b8
Radmila Gama <milaejared@outlook.com>
2021-01-21 17:21:32 -0300

    Ok?

--- Modulo 3/1- Aplicacao/Controllers/CategoriasController.cs
+++ Modulo 3/1- Aplicacao/Controllers/CategoriasController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _1__Aplicacao.Models;
 
 namespace _1__Aplicacao.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() { CategoriaId = 1, Nome = "Notebooks"},
+            new Categoria() { CategoriaId = 2, Nome = "Monitores"},
+            new Categoria() { CategoriaId = 3, Nome = "Impressoras"},
+            new Categoria() { CategoriaId = 4, Nome = "Mouses"},
+            new Categoria() { CategoriaId = 5, Nome = "Desktops"}
+        };
+
         // GET: Categorias
         public ActionResult Index()
         {
-            return View();
+            return View(categorias);
         }
     }
 }
