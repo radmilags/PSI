2b6dd6245ced942afd69b03ae3fc74e9a0837c44
Radmila Gama <milaejared@outlook.com>
2021-01-28 15:41:55 -0300

    Finalmente deu certo

---
+++ Modulo 3/1- Aplicacao/Context/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using _1__Aplicacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _1__Aplicacao.Context
+{
+    public class EFContext: System.Data.Entity.DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
