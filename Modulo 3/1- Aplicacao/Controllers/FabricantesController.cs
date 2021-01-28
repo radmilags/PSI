bbd34c9c80e1c9ce9f1447c6fe56173808dd659f
Radmila Gama <milaejared@outlook.com>
2021-01-28 16:17:25 -0300

    Preciso instalar o SQL

---
+++ Modulo 3/1- Aplicacao/Controllers/FabricantesController.cs
@@ -0,0 +1,20 @@
+﻿using _1__Aplicacao.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1__Aplicacao.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
