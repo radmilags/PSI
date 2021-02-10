ca0275ba96e5eb789d93f3f8219fe5d36dda48e9
Radmila Gama <milaejared@outlook.com>
2021-02-10 18:50:19 -0300

    ok

--- Modulo 3/1- Aplicacao/Controllers/FabricantesController.cs
+++ Modulo 3/1- Aplicacao/Controllers/FabricantesController.cs
@@ -102,6 +102,7 @@ namespace _1__Aplicacao.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
