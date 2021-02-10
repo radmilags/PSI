d2923f29d2235129e766e84bf76591b60976ec36
Radmila Gama <milaejared@outlook.com>
2021-02-10 20:47:01 -0300

    ok

--- Modulo 3/1- Aplicacao/Context/EFContext.cs
+++ Modulo 3/1- Aplicacao/Context/EFContext.cs
@@ -12,5 +12,6 @@ namespace _1__Aplicacao.Context
         public EFContext() : base("Asp_Net_MVC_CS") { }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
