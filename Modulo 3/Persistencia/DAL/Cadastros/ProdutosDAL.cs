1915d83595242ea0069b13d96a6da6c6acb0f984
Radmila Gama <milaejared@outlook.com>
2021-03-11 14:21:46 -0300

    ok

--- Modulo 3/Persistencia/DAL/Cadastros/ProdutosDAL.cs
+++ Modulo 3/Persistencia/DAL/Cadastros/ProdutosDAL.cs
@@ -18,5 +18,24 @@ namespace Persistencia.DAL.Cadastros
             return context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).
             Include(f => f.Fabricante).First();
         }
+        public void GravarProduto(Produto produto)
+        {
+            if (produto.ProdutoId == null)
+            {
+                context.Produtos.Add(produto);
+            }
+            else
+            {
+                context.Entry(produto).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+        public Produto EliminarProdutoPorId(long id)
+        {
+            Produto produto = ObterProdutoPorId(id);
+            context.Produtos.Remove(produto);
+            context.SaveChanges();
+            return produto;
+        }
     }
 }
