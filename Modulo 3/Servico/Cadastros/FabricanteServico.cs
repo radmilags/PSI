64643a095b01ac6fbaceef9aec82a17980c61623
Radmila Gama <milaejared@outlook.com>
2021-03-15 14:21:42 -0300

    ok

---
+++ Modulo 3/Servico/Cadastros/FabricanteServico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Cadastros;
+using Persistencia.DAL.Cadastros;
+
+namespace Servico.Cadastros
+{
+    class FabricanteServico
+    {
+        private FabricanteDAL fabricanteDAL = new FabricanteDAL();
+        public IQueryable<Fabricante> ObterFabricantesClassificadosPorNome()
+        {
+            return fabricanteDAL.ObterFabricantesClassificadosPorNome();
+        }
+    }
+}
