f4069acd31b5d4d6846dfa138de86dbbd609e3ba
Radmila Gama <milaejared@outlook.com>
2021-03-22 21:21:12 -0300

    ok?

--- Modulo 3/Persistencia/Context/EFContext.cs
+++ Modulo 3/Persistencia/Context/EFContext.cs
@@ -1,30 +1,29 @@
-﻿using Modelo.Cadastros;
-using Modelo.Tabelas;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using Modelo.Cadastros;
+using Modelo.Tabelas;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Persistencia.Migrations;
 
-namespace Persistencia.Context
+namespace Persistencia.Contexts
 {
-    public class EFContext : System.Data.Entity.DbContext
+    public class EFContext : DbContext
     {
         public EFContext() : base("Asp_Net_MVC_CS")
         {
-            Database.SetInitializer<EFContext>(
-            new DropCreateDatabaseIfModelChanges<EFContext>());
+            Database.SetInitializer<EFContext>(new MigrateDatabaseToLatestVersion<EFContext, Configuration>());
         }
-
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
-
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     }
+
 }
