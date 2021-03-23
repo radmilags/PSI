b70d241429773280880dd26635ab5b7ea32bb478
Radmila Gama <milaejared@outlook.com>
2021-03-22 21:28:37 -0300

    sempre dando errado

--- Modulo 3/Persistencia/DAL/Cadastros/FabricanteDAL.cs
+++ Modulo 3/Persistencia/DAL/Cadastros/FabricanteDAL.cs
@@ -1,6 +1,11 @@
-﻿using Persistencia.Context;
-using Modelo.Cadastros;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia.Contexts;
+using Modelo.Cadastros;
+using System.Data.Entity;
 
 namespace Persistencia.DAL.Cadastros
 {
@@ -10,6 +15,29 @@ namespace Persistencia.DAL.Cadastros
         public IQueryable<Fabricante> ObterFabricantesClassificadosPorNome()
         {
             return context.Fabricantes.OrderBy(b => b.Nome);
+        }
+        public Fabricante ObterFabricantePorId(long id)
+        {
+            return context.Fabricantes.Where(f => f.FabricanteId == id).First();
+        }
+        public void GravarFabricante(Fabricante fabricante)
+        {
+            if (fabricante.FabricanteId == 0)
+            {
+                context.Fabricantes.Add(fabricante);
+            }
+            else
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+        public Fabricante EliminarFabricantePorId(long id)
+        {
+            Fabricante fabricante = ObterFabricantePorId(id);
+            context.Fabricantes.Remove(fabricante);
+            context.SaveChanges();
+            return fabricante;
         }
     }
 }
--- Modulo 3/Persistencia/DAL/Cadastros/ProdutoDAL.cs
+++ Modulo 3/Persistencia/DAL/Cadastros/ProdutoDAL.cs
@@ -1,7 +1,11 @@
-﻿using Persistencia.Context;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia.Contexts;
 using System.Data.Entity;
 using Modelo.Cadastros;
-using System.Linq;
 
 namespace Persistencia.DAL.Cadastros
 {
@@ -10,13 +14,11 @@ namespace Persistencia.DAL.Cadastros
         private EFContext context = new EFContext();
         public IQueryable<Produto> ObterProdutosClassificadosPorNome()
         {
-            return context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).
-            OrderBy(n => n.Nome);
+            return context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).OrderBy(n => n.Nome);
         }
         public Produto ObterProdutoPorId(long id)
         {
-            return context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).
-            Include(f => f.Fabricante).First();
+            return context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
         }
         public void GravarProduto(Produto produto)
         {
