64d37ee9a49a8e73ec6f3d4b52904dcc31c91ff9
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-04-24 18:58:00 +0000

    chore: organização dos arquivos

---
+++ Modulo 3/Servico/Cadastros/FabricanteServico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Cadastros;
+using Persistencia.DAL.Cadastros;
+
+namespace Servico.Cadastros
+{
+    public class FabricanteServico
+    {
+        private FabricanteDAL fabricanteDAL = new FabricanteDAL();
+        public IQueryable<Fabricante> ObterFabricantesClassificadosPorNome()
+        {
+            return fabricanteDAL.ObterFabricantesClassificadosPorNome();
+        }
+        public Fabricante ObterFabricantePorId(long id)
+        {
+            return fabricanteDAL.ObterFabricantePorId(id);
+        }
+        public void GravarFabricante(Fabricante fabricante)
+        {
+            fabricanteDAL.GravarFabricante(fabricante);
+        }
+        public Fabricante EliminarFabricantePorId(long id)
+        {
+            return fabricanteDAL.EliminarFabricantePorId(id);
+        }
+    }
+}
